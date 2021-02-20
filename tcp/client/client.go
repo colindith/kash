30a4648c77f35253e0225617b67c8c4b781253ac
colindith <siegcalu@gmail.com>
2021-02-20 21:30:31 +0800

    add tcp client

---
+++ tcp/client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
+
+// TODO: The client package should start a cmd line process and let users type the cmd in the cmd line.
+// TODO: The cmd line should be provide some basic functions like using upper arrow key to find the history and and using left/right arrow to move the cursor
+
+func main() {
+
+}
+
+// SendTCPCmd send the cmd to the remote tcp server and close the connection immediately
+func SendTCPCmd(host string, port string, cmd string) {
+	// TODO: should maintain a TCP connection pool, instead of create new connection everytime
+	// TODO: The conn pool can be configured with "max_active", "min_active", "active_timeout"
+	conn, err := net.DialTimeout("tcp", host+ ":" + port, 10 * time.Second)
+	if err != nil {
+		fmt.Println("dail_to_tcp_server_err | err=", err.Error())
+		return
+	}
+	defer conn.Close()
+
+	_, err = fmt.Fprintf(conn, cmd + "\n")
+	if err != nil {
+		fmt.Println("tcp_write_err | err=", err.Error())
+		return
+	}
+	resp, err := bufio.NewReader(conn).ReadString('\n')
+	if resp != "OK\n" {
+		fmt.Println("NOT_OK | msg=", resp)
+	}
+}
---
+++ tcp/client/client_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"testing"
+)
+
+func Test_SendTCPCmd(t *testing.T) {
+	l ,err := testTCPServer("localhost", "3333")
+	if err != nil {
+		t.Errorf("test_tcp_server_err | err=%v", err.Error())
+	}
+	SendTCPCmd("localhost", "3333", "11223344")
+	l.Close()
+}
+
+// testTCPServer run a tcp server and and only serve one request from one client
+func testTCPServer(host string, port string) (l net.Listener, err error) {
+	l, err = net.Listen("tcp", host + ":" + port)
+	if err != nil {
+		log.Fatalf("net_listen_error | err=%v", err.Error())
+		return l, err
+	}
+	log.Printf("kash_server_listen_at | %v", host + ":" + port)
+
+	go func() {
+		//for {
+			c, err := l.Accept()
+			if err != nil {
+				log.Fatalf("net_accept_error | err=%v", err.Error())
+				return
+			}
+
+			//for {
+				netData, err := bufio.NewReader(c).ReadBytes('\n')
+				if err != nil {
+					log.Fatalf("bufio_read_bytes_error | err=%v", err.Error())
+					return
+				}
+
+				if string(netData) != "11223344\n" {
+					//return errors.New(fmt.Sprintf("receiving_data_incorrect | received=%v | want=%v", string(netData), "11223344\n"))
+					log.Fatalf("receiving_data_incorrect | received=%v | want=%v", string(netData), "11223344\n")
+					return
+				}
+
+				// Always response "OK\n"
+				_, err = c.Write([]byte("OK\n"))
+				if err != nil {
+					log.Fatalf("net_connection_write_error | err=%v", err.Error())
+				}
+			//}
+			c.Close()
+		//}
+	}()
+	return
+}
