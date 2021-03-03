aa8dad2815ec8cff581df6cfee40693c538e0db9
Colindith <siegcalu@gmail.com>
2021-03-04 00:22:18 +0800

    modify SendTCPCmd

--- client/client.go
+++ client/client.go
@@ -4,10 +4,16 @@ import (
 	"log"
 
 	"github.com/colindith/kash/cmd_reader"
+	"github.com/colindith/kash/tcp"
 )
 
 // TODO: The client package should start a cmd line process and let users type the cmd in the cmd line.
 // TODO: The cmd line should be provide some basic functions like using upper arrow key to find the history and and using left/right arrow to move the cursor
+
+const (           // TODO: the host & port should be passed in from outside
+	connHost = "localhost"
+	connPort = "3333"
+)
 
 func main() {
 	cfg := &cmd_reader.Config{}
@@ -17,10 +23,11 @@ func main() {
 		return
 	}
 
+	// TODO: should connect to remote server before running the cli
 	cmd_reader.Run(cfg)
 }
 
 var handler = &cmd_reader.Handler{Serv: func(cmd string) (result string, err error) {
-
-	return "", nil
+	result = tcp.SendTCPCmd(connHost, connPort, cmd)
+	return result, nil
 }}
--- tcp/client.go
+++ tcp/client.go
@@ -3,28 +3,30 @@ package tcp
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"time"
 )
 
 // SendTCPCmd send the cmd to the remote tcp server and close the connection immediately
-func SendTCPCmd(host string, port string, cmd string) {
+func SendTCPCmd(host string, port string, cmd string) string {
 	// TODO: should maintain a TCP connection pool, instead of create new connection everytime
 	// TODO: The conn pool can be configured with "max_active", "min_active", "active_timeout"
 	conn, err := net.DialTimeout("tcp", host+ ":" + port, 10 * time.Second)
 	if err != nil {
-		fmt.Println("dail_to_tcp_server_err | err=", err.Error())
-		return
+		log.Printf("dail_to_tcp_server_err | err=%v", err.Error())
+		return "error..."
 	}
 	defer conn.Close()
 
 	_, err = fmt.Fprintf(conn, cmd + "\n")
 	if err != nil {
-		fmt.Println("tcp_write_err | err=", err.Error())
-		return
+		log.Printf("tcp_write_err | err=%v", err.Error())
+		return "error..."
 	}
 	resp, err := bufio.NewReader(conn).ReadString('\n')
 	if resp != "OK\n" {
-		fmt.Println("NOT_OK | msg=", resp)
+		log.Printf("NOT_OK | msg=%v", resp)
 	}
+	return resp
 }
